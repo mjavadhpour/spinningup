abb41b4ef9c733e41117899d50c8c24b0b749100
Mohammad Javad Hosein Pour <mjavadhpour@gmail.com>
2022-01-06 14:32:00 +0330

    chore: move IDE packages to conda package vs site-packages

--- setup.py
+++ setup.py
@@ -28,15 +28,7 @@ setup(
         'seaborn==0.8.1',
         'tensorflow>=1.8.0,<2.0',
         'torch==1.3.1',
-        'tqdm',
-        # Python IDE Dependencies
-        'pylint',
-        'pydocstyle',
-        'pycodestyle',
-        'flake8',
-        'yapf',
-        'autopep8'
-        # End
+        'tqdm'
     ],
     description="Teaching tools for introducing people to deep RL.",
     author="Joshua Achiam",
